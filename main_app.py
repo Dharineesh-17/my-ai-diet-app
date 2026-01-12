41db1924190aa5bdff64c2f64558cf56fb9252cb
Dharineesh-17 <dharineeshvengatesan@gmail.com>
2026-01-12 15:33:29 +0530

    Update AI model from 'gemini-pro' to 'gemini-1.5-flash'

--- main_app.py
+++ main_app.py
@@ -17,7 +17,7 @@ def create_pdf(text):
 # --- 3. AI CONFIGURATION (Using Secrets) ---
 try:
     genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
-      model = genai.GenerativeModel('gemini-pro')
+    model = genai.GenerativeModel('gemini-1.5-flash')
 except Exception as e:
     st.error("API Key missing! Please add it to Streamlit Secrets.")
 
