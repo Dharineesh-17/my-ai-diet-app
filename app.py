0b2141374d4f24de407d6acf1ab93873b6607460
Dharineesh-17 <dharineeshvengatesan@gmail.com>
2026-02-02 10:31:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ st.markdown("""
 
         background-color: #f8fafc !important;
 
-        border-radius: 10px 10px 0 0 !important;
+        border-radius: 30px 30px 0 0 !important;
 
         margin-right: 5px !important;
 
